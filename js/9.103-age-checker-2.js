4bfb1ca8d4e81344636e64e3c20a2f46b135dd7f
Mike Smith <mk.smth@gmail.com>
2018-08-29 11:34:23 +0100

    second age checker files added

---
+++ js/9.103-age-checker-2.js
@@ -0,0 +1,20 @@
+var age = prompt("How old are you?");
+
+
+if(age < 0) {
+  document.write("You cannot possibly be serious!!");
+}
+else if(age == 21) {
+  document.write("Happy 21st Birthday :)!!");
+}
+else if (age % Math.sqrt(age) === 0) {
+  document.write("Perfect square!");
+}
+else if(age % 2 === 1) {
+  document.write("Hey, your age is odd!!");
+}
+
+
+else {
+  document.write("Wow you are " + age + " years old!");
+}
