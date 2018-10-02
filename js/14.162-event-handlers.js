d54e6e6f22dbbbb25dceb33bbae477ef79b74bef
Mike Smith <mk.smth@gmail.com>
2018-10-02 12:47:52 +0100

    refactored to use classList.toggle

--- js/14.162-event-handlers.js
+++ js/14.162-event-handlers.js
@@ -1,12 +1,18 @@
+// var button = document.querySelector("button");
+// var panel = document.querySelector("body");
+
+// button.addEventListener("click", function(){
+//   if (panel.style.background == "purple") {
+//     panel.style.background = null;
+//   }
+//   else {
+//     panel.style.background = "purple";
+//   }
+// });
+
 var button = document.querySelector("button");
-var panel = document.querySelector("body");
 
-button.addEventListener("click", function(){
-  if (panel.style.background == "purple") {
-    panel.style.background = null;
-  }
-  else {
-    panel.style.background = "purple";
-  }
-});
-
+button.addEventListener("click", function() {
+  document.body.classList.toggle("purple");
+  
+})
