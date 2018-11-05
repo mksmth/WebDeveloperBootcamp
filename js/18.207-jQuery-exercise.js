4fa55cf87b13df043e9381010a6f0d80e9af2f37
Mike Smith <mk.smth@gmail.com>
2018-11-05 15:59:37 +0000

    alternate versions

--- js/18.207-jQuery-exercise.js
+++ js/18.207-jQuery-exercise.js
@@ -2,6 +2,9 @@ $("div").css("background", "purple");
 
 $(".highlight").css("width", "200px");
 
-$("#third").css({border: "2px solid orange"});
+// $("#third").css({border: "2px solid orange"});
+$("#third").css("border", "2px solid orange");
 
-$("div").first().css("color", "pink");
+
+// $("div").first().css("color", "pink");
+$("div:first-of-type").css("color", "pink");
