6def8cf49ea02f4a586bfea395ca88fb6602d7a9
Mike Smith <mk.smth@gmail.com>
2018-11-30 11:58:23 +0000

    forEach version added to grader

--- udemy-22-243/www/IntroToNode/grader.js
+++ udemy-22-243/www/IntroToNode/grader.js
@@ -2,11 +2,11 @@ var scores = [90, 98, 89, 100, 100, 86, 94];
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
 
 
-function average(arr){
+function average(score){
   var total = 0;
-  for(var i = 0; i < arr.length; i++){
-    total += arr[i];
-    var mean = Math.round(total / arr.length);
+  for(var i = 0; i < score.length; i++){
+    total += score[i];
+    var mean = Math.round(total / score.length);
     }
     return mean;
 }
@@ -14,3 +14,14 @@ function average(arr){
 console.log("Average of scores is: " + average(scores));
 console.log("Average of scores2 is: " + average(scores2));
 
+function average2(scores) {
+  var total = 0;
+  scores.forEach(function(score){
+    total += score;
+    avg = Math.round(total / scores.length);
+  });
+  return avg
+}
+
+console.log("Average of scores is: " + average2(scores));
+console.log("Average of scores2 is: " + average2(scores2));
