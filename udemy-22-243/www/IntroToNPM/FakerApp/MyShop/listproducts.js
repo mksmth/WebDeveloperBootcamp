c6aa08cd6f216ef0eb9460ad71710b9bfc99d62f
Mike Smith <mk.smth@gmail.com>
2018-11-30 18:19:45 +0000

    dollar sign added to faker price list

--- udemy-22-243/www/IntroToNPM/FakerApp/MyShop/listproducts.js
+++ udemy-22-243/www/IntroToNPM/FakerApp/MyShop/listproducts.js
@@ -7,5 +7,5 @@ console.log("====================");
 for(var i = 0; i < 10; i++) {
 var productName = faker.commerce.productName();
 var productPrice = faker.commerce.price();
-console.log(productName + " - " + productPrice);
+console.log(productName + " - $" + productPrice);
 }
