19e1ce5a0c6d69fbbd0df2df02c44f996ea48651
Mike Smith <mk.smth@gmail.com>
2018-12-05 15:51:35 +0000

    Express routes with params added

--- udemy-22-243/www/IntroToExpress/FirstExpressApp/app.js
+++ udemy-22-243/www/IntroToExpress/FirstExpressApp/app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+// const port = "3001";
+
 
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
@@ -7,7 +9,7 @@ app.get("/", function(req, res){
 	res.send("Hi There!");
 });
 
-app.listen(3000);
+app.listen(3001);
 
 app.get("/bye", function(req, res){
 	res.send("Goodbye!!");
@@ -15,5 +17,10 @@ app.get("/bye", function(req, res){
 app.get("/dog", function(req, res){
 	res.send("MEEOOOW!!!!");	
 });
+app.get("/hello/:pattern/bye/:pattern2", function(req, res){
+	var pattern = req.params.pattern.toUpperCase();
+	var pattern2 = req.params.pattern2.toUpperCase(); 
+	res.send("Hello from a pattern route called " + pattern +". And goodbye from " + pattern2 + ".");
+});
 
-app.get("*", (req, res) => res.sendStatus(404));
+// app.get("*", (req, res) => res.sendStatus(404));
