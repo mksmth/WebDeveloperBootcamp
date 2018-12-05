3753ca1a7c9f31bda3882fe66aead2f90f14ad18
Mike Smith <mk.smth@gmail.com>
2018-12-05 16:48:55 +0000

    Express Exercise added

---
+++ udemy-22-243/www/IntroToExpress/ExpressExercise/app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+app.listen(3000);
+
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
+
+app.get("/", function(req, res){
+  res.send("Hi there, welcome to my assignment!");
+});
+
+app.get("/speak/:animal", function(req, res){
+  var animal = req.params.animal;
+  if(animal === "pig"){
+    var sound = "'Oink'";
+  } else if(animal === "cow"){
+    var sound = "'Moo'";
+  } else if(animal === "dog"){
+    var sound = "'Woof, Woof!'";
+  } else {
+    var sound = "'Hello!!'";
+  }
+  res.send("The " + animal + " says " + sound);
+});
+
+app.get("/repeat/:greeting/:num", function(req, res){
+  var greeting = req.params.greeting + " ";
+  var num = req.params.num;
+  res.send(greeting.repeat(num));
+});
+
+app.get("*", (req, res) => res.sendStatus(404));
