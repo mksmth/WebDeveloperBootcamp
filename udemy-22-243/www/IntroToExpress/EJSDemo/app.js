adb346b6f821d9cfad1b85dca44010125ddbd0aa
Mike Smith <mk.smth@gmail.com>
2018-12-10 17:16:08 +0000

    partials and includes

--- udemy-22-243/www/IntroToExpress/EJSDemo/app.js
+++ udemy-22-243/www/IntroToExpress/EJSDemo/app.js
@@ -1,15 +1,27 @@
 var express = require("express");
 var app = express();
 app.listen(3000);
+app.use(express.static("public"));
 
+app.set("view engine", "ejs");
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/everfalleninlovewith/:someone", function(req,res) {
   var someone = req.params.someone;
-  res.render("love.ejs");
+  res.render("love", {someoneVar: someone});
+});
+
+app.get("/posts", function(req, res){
+  var posts = [
+  {title: "Post 1", author: "Susy"},
+  {title: "Jeeves and Wooster", author: "Bunter"},
+  {title: "Can you believe this Alfie?", author: "Mike"},
+  {title: "C'mon Everybody", author: "Eddie Cochran"},
+  {title: "Watership Down", author: "Richard Adams"}
+  ]
 });
 
