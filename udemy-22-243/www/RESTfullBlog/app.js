ea302d4fcb5ae558ef77b5ef7454bc3f335e4a5d
Mike Smith <mk.smth@gmail.com>
2019-01-15 17:07:16 +0000

    blog app post working with basic approach; course instruction to use [blog]title etc in form and app js route fails with error. To do: find workaround that fits course instruciton

--- udemy-22-243/www/RESTfullBlog/app.js
+++ udemy-22-243/www/RESTfullBlog/app.js
@@ -20,20 +20,20 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create(
-  {
-    title: "My first blog post",
-    image: "https://images.unsplash.com/photo-1470425056423-3e7d20e2fd50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    body: "Here's some blether about a picture of a terrier."
+// Blog.create(
+//   {
+//     title: "My first blog post",
+//     image: "https://images.unsplash.com/photo-1470425056423-3e7d20e2fd50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+//     body: "Here's some blether about a picture of a terrier."
 
-  }, function(err, blog){
-    if(err){
-      console.log(err);
-    } else {
-      console.log("NEW BLOGPOST!!:");
-      console.log(blog);
-      }
-  });
+//   }, function(err, blog){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       console.log("NEW BLOGPOST!!:");
+//       console.log(blog);
+//       }
+//   });
 
 
 // RESTFULL ROUTES
@@ -42,6 +42,7 @@ app.get("/", function(req, res){
   res.redirect("/blogs");
 });
 
+//INDEX: displays all blogs in listing
 app.get("/blogs", function(req, res){
   // Get all blogs from db
 	Blog.find({}, function(err, blogs){
@@ -52,3 +53,26 @@ app.get("/blogs", function(req, res){
       }
     });
   });
+
+
+// NEW: displays form to add new blog to DB
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+
+//CREATE: saves a new blog post to the db
+
+app.post("/blogs", function(req, res) {
+  var title = req.body.title;
+  var image = req.body.image;
+  var body = req.body.body;
+  var newBlog = {title: title, image: image, body: body};
+  // Create new campsite and save to db
+  Blog.create(newBlog, function(err, newlyCreated){
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("blogs");
+    }
+  });
+});
