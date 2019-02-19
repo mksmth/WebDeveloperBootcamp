8620332a4a7d32d519e7da001d6e3435f03edd87
Mike Smith <mk.smth@gmail.com>
2019-02-19 14:13:23 +0000

    yelpcamp v8 created and comment author name added to model and show view

---
+++ udemy-22-243/www/Yelpcamp/v8/app.js
@@ -0,0 +1,49 @@
+var express               = require("express"),
+    app                   = express(),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    LocalStrategy         =  require("passport-local"),
+    Campsite              = require("./models/campsite"),
+    Comment               = require("./models/comment"),
+    User                  = require("./models/user"),
+    seedDB                = require("./seeds");
+
+var indexRoutes           = require("./routes/index"),
+    campsiteRoutes        = require("./routes/campsites"),
+    commentRoutes         = require("./routes/comments");
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/yelpcamp_v8", {useNewUrlParser: true});
+
+app.listen(3008);
+
+//SEED DB
+// seedDB();
+
+//PASSPORT CONFIG
+
+app.use(require("express-session")({
+  secret: "Alfie is the cutest dog",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(indexRoutes);
+app.use("/campsites", campsiteRoutes);
+app.use("/campsites/:id/comments", commentRoutes);
+
+
---
+++ udemy-22-243/www/Yelpcamp/v8/seeds.js
@@ -0,0 +1,70 @@
+var mongoose = require("mongoose");
+var Campsite = require("./models/campsite");
+var Comment = require("./models/comment");
+//ADD A FEW CAMPSITES
+
+var data = [
+  {
+    name: "Grizedale", 
+    image: "https://assets.bedful.com/images/407af1e9acee5bc371e4af51de0ba2dea1fc4cc1/large.jpg", 
+    description: "Lovely campsite in the Lakes"
+  },
+  {
+    name: "Helvellyn", 
+    image: "http://3.bp.blogspot.com/-xw1hQ507DTM/UCpQiqJ_HfI/AAAAAAAABRg/6a7TWhRQnYc/s1600/9.JPG", 
+    description: "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead."
+  },
+  {
+    name: "Catbells", 
+    image: "http://www.campingwithstyle.co.uk/wp-content/uploads/2017/09/ridge-to-catbells.jpg", 
+    description: "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead."
+  },
+  {
+    name: "Braithwaite", 
+    image: "https://media-cdn.tripadvisor.com/media/photo-s/08/e6/26/a0/scotgate-holiday-park.jpg", 
+    description: "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead."
+  }
+ ];
+
+function seedDB(){
+  Campsite.deleteMany({}, function(err){
+    if(err) {
+      console.log(err);
+    } 
+    console.log("Campsites removed");
+  Comment.deleteMany({}, function(err){
+    if(err) {
+      console.log(err);
+    } 
+    console.log("Comments removed");
+    });
+    // data.forEach(function(seed){
+    //   Campsite.create(seed, function(err, campsite){
+    //     if(err){
+    //       console.log(err);
+    //     } else {
+    //       console.log("Added a Campsite");
+    //       Comment.create(
+    //         {
+    //           text: "This place is beautiful",
+    //           author: "HomersOdyssey"
+    //          },
+    //           function(err, comments){
+    //               if(err){
+    //                 console.log(err);
+    //               } else {
+    //                 campsite.comments.push(comments);
+    //                 campsite.save();
+    //                 console.log("Added a comment");
+    //               }
+    //          })
+    //     }
+    //   });
+    // });
+  });   
+  }
+
+  
+//ADD A FEW COMMENTS
+
+module.exports = seedDB;
